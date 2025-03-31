e417fb7eb52fb097b41db8eb49a49f003a68c4fd
Alex-dev-eng <sigutkinaleksej@gmail.com>
2025-03-31 22:54:25 +0300

    letuchka

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
@@ -5,8 +5,13 @@
 std::vector<int> positive_numbers(const std::vector<int>& v)
 {
 	std::vector<int> result;
-	std::copy_if(v.begin(), v.end(), std::back_inserter(result),
-				 [](int num) { return num > 0; });
+	for (int num : v)
+	{
+		if (num > 0)
+		{
+			result.push_back(num);
+		}
+	}
 	return result;
 }
 
@@ -17,26 +22,46 @@ void sort_positive_numbers(std::vector<int>& v)
 
 int sum_positive_numbers(const std::vector<int>& v)
 {
-	return std::accumulate(v.begin(), v.end(), 0, [](int sum, int num)
-						   { return num > 0 ? sum + num : sum; });
+	int sum = 0;
+	for (int num : v)
+	{
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+	return sum;
 }
 
 bool is_divisible_by_10(const std::vector<int>& v)
 {
-	return std::all_of(v.begin(), v.end(),
-					   [](int num) { return num % 10 == 0; });
+	for (int num : v)
+	{
+		if (num % 10 != 0)
+		{
+			return false;
+		}
+	}
+	return !v.empty();
 }
 
 void replace_negative_numbers(std::vector<int>& v)
 {
-	std::transform(v.begin(), v.end(), v.begin(),
-				   [](int num) { return num < 0 ? 0 : num; });
+	for (int& num : v)
+	{
+		if (num < 0)
+		{
+			num = 0;
+		}
+	}
 }
 
 void double_values(std::vector<int>& v)
 {
-	std::transform(v.begin(), v.end(), v.begin(),
-				   [](int num) { return num * 2; });
+	for (int& num : v)
+	{
+		num *= 2;
+	}
 }
 
 void sort_students_by_age(std::vector<Student>& v)
